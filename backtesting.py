dd1b96606eee8583be76337aa10eeb0218be6286
poArlim <dkfhscjsgk96@gmail.com>
2021-04-22 23:03:58 +0900

    add comments

--- backtesting.py
+++ backtesting.py
@@ -10,7 +10,7 @@ import time
 coin = "KRW-BTC"
 interval = "day"
 fees = 0.0005
-day_count = 1003
+day_count = 1305
 
 date = None
 dfs = [ ]
@@ -29,7 +29,7 @@ for i in range(day_count // 200 + 1):
 df = pd.concat(dfs).sort_index()
 
 df['range'] = df['high'].shift(1) - df['low'].shift(1)
-df['targetPrice'] = df['open'] + df['range'].shift(1) * 0.5
+df['targetPrice'] = df['open'] + df['range'].shift(1) * 0.28
 df['drr'] = np.where(df['high'] > df['targetPrice'], (df['open'].shift(-1) / (1 + fees)) / (df['targetPrice'] * (1 + fees)) - 1, 0)
 
 df['crr'] = 1.0
@@ -41,5 +41,5 @@ for i in range(2, day_count) :
     df['mdd'][i] = (df['crr'][i] / df['max_crr'][i]) - 1
 
 print("기간수익률 :", df['crr'][-1] * 100 - 100, "% , 최대손실률 :", min(df['mdd']) * 100, "% , 수수료 :", fees * 100, "%")
-
+print("알고리즘 적용 없을 시 수익률 :", ((df['close'][-1]/(1+fees))/(df['open'][0]*(1+fees))-1) * 100,"%")
 df.to_excel("btc_history.xlsx")
