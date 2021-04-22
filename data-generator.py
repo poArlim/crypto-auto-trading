fbdf58f4ba67183bbaae2bc82abd3aa64656e5ac
poArlim <dkfhscjsgk96@gmail.com>
2021-04-22 21:04:50 +0900

    complete data-generator.py

--- data-generator.py
+++ data-generator.py
@@ -1,11 +1,16 @@
 import pyupbit
 import pandas as pd
+import numpy as np
 import time
+
+# drr = Daily Rate Of Return
+# crr = Cumulative Rate Of Return
+# mdd = Max Draw Down
 
 coin = "KRW-BTC"
 interval = "day"
 fees = 0.0005
-day_count = 1000
+day_count = 1003
 
 date = None
 dfs = [ ]
@@ -22,4 +27,19 @@ for i in range(day_count // 200 + 1):
     time.sleep(0.1)
 
 df = pd.concat(dfs).sort_index()
+
+df['range'] = df['high'].shift(1) - df['low'].shift(1)
+df['targetPrice'] = df['open'] + df['range'].shift(1) * 0.5
+df['drr'] = np.where(df['high'] > df['targetPrice'], (df['open'].shift(-1) / (1 + fees)) / (df['targetPrice'] * (1 + fees)) - 1, 0)
+
+df['crr'] = 1.0
+df['mdd'] = 0.0
+df['max_crr'] = 1.0
+for i in range(2, day_count) :
+    df['crr'][i] = df['crr'][i-1] * (1 + df['drr'][i])
+    df['max_crr'][i] = max(df['max_crr'][i-1], df['crr'][i])
+    df['mdd'][i] = (df['crr'][i] / df['max_crr'][i]) - 1
+
+print("기간수익률 :", df['crr'][-1] * 100 - 100, "% , 최대손실률 :", min(df['mdd']) * 100, "% , 수수료 :", fees * 100, "%")
+
 df.to_excel("btc_history.xlsx")
