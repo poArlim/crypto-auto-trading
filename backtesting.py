762136631679e1593a43a42012e533c6f184fc25
poArlim <dkfhscjsgk96@gmail.com>
2021-04-24 03:16:29 +0900

    fix targetPrice fomula

--- backtesting.py
+++ backtesting.py
@@ -10,7 +10,8 @@ import time
 coin = "KRW-BTC"
 interval = "day"
 fees = 0.0005
-day_count = 1305
+day_count = 1300
+K = 0.5
 
 date = None
 dfs = [ ]
@@ -29,17 +30,18 @@ for i in range(day_count // 200 + 1):
 df = pd.concat(dfs).sort_index()
 
 df['range'] = df['high'].shift(1) - df['low'].shift(1)
-df['targetPrice'] = df['open'] + df['range'].shift(1) * 0.28
-df['drr'] = np.where(df['high'] > df['targetPrice'], (df['open'].shift(-1) / (1 + fees)) / (df['targetPrice'] * (1 + fees)) - 1, 0)
+df['targetPrice'] = df['open'] + df['range'] * K
+df['drr'] = np.where(df['high'] > df['targetPrice'], (df['close'] / (1 + fees)) / (df['targetPrice'] * (1 + fees)) - 1, 0)
 
 df['crr'] = 1.0
 df['mdd'] = 0.0
 df['max_crr'] = 1.0
-for i in range(2, day_count) :
+for i in range(1, day_count) :
     df['crr'][i] = df['crr'][i-1] * (1 + df['drr'][i])
     df['max_crr'][i] = max(df['max_crr'][i-1], df['crr'][i])
     df['mdd'][i] = (df['crr'][i] / df['max_crr'][i]) - 1
 
 print("기간수익률 :", df['crr'][-1] * 100 - 100, "% , 최대손실률 :", min(df['mdd']) * 100, "% , 수수료 :", fees * 100, "%")
 print("알고리즘 적용 없을 시 수익률 :", ((df['close'][-1]/(1+fees))/(df['open'][0]*(1+fees))-1) * 100,"%")
-df.to_excel("btc_history.xlsx")
+
+df.to_excel("crypto_history.xlsx")
