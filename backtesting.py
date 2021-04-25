a3f0b5cbcdd1fef6c57c7da31d913a740c72e420
poArlim <dkfhscjsgk96@gmail.com>
2021-04-26 03:35:26 +0900

    fix mdd to dd

--- backtesting.py
+++ backtesting.py
@@ -5,7 +5,7 @@ import time
 
 # drr = Daily Rate Of Return
 # crr = Cumulative Rate Of Return
-# mdd = Max Draw Down
+# mdd = Max Draw Down, dd = Draw Down
 
 coin = "KRW-BTC"
 interval = "day"
@@ -24,7 +24,7 @@ for i in range(day_count // 200 + 1):
         df = pyupbit.get_ohlcv(coin, to = date, interval = interval, count = day_count % 200)
     else :
         break
-    dfs.append(df)s
+    dfs.append(df)
     time.sleep(0.1)
 
 df = pd.concat(dfs).sort_index()
@@ -34,9 +34,9 @@ df['targetPrice'] = df['open'] + df['range'] * K
 df['drr'] = np.where(df['high'] > df['targetPrice'], (df['close'] / (1 + fees)) / (df['targetPrice'] * (1 + fees)) - 1 , 0)
 
 df['crr'] = (df['drr'] + 1).cumprod() - 1
-df['mdd'] = -(((df['crr'] + 1).cummax() - (df['crr'] + 1)) / (df['crr'] + 1).cummax())
+df['dd'] = -(((df['crr'] + 1).cummax() - (df['crr'] + 1)) / (df['crr'] + 1).cummax())
 
-print("기간수익률 :", df['crr'][-1] * 100 - 100, "% , 최대손실률 :", df['mdd'].min() * 100, "% , 수수료 :", fees * 100, "%")
+print("기간수익률 :", df['crr'][-1] * 100 - 100, "% , 최대손실률 :", df['dd'].min() * 100, "% , 수수료 :", fees * 100, "%")
 print("알고리즘 적용 없을 시 수익률 :", ((df['close'][-1]/(1+fees))/(df['open'][0]*(1+fees))-1) * 100,"%")
 
 df.to_excel("crypto_history.xlsx")
