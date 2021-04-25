ec9e51348969fe36d174ebfc5e3d97e428e177b7
poArlim <dkfhscjsgk96@gmail.com>
2021-04-26 01:27:06 +0900

    replace for loop to np function

--- backtesting.py
+++ backtesting.py
@@ -24,24 +24,19 @@ for i in range(day_count // 200 + 1):
         df = pyupbit.get_ohlcv(coin, to = date, interval = interval, count = day_count % 200)
     else :
         break
-    dfs.append(df)
+    dfs.append(df)s
     time.sleep(0.1)
 
 df = pd.concat(dfs).sort_index()
 
 df['range'] = df['high'].shift(1) - df['low'].shift(1)
 df['targetPrice'] = df['open'] + df['range'] * K
-df['drr'] = np.where(df['high'] > df['targetPrice'], (df['close'] / (1 + fees)) / (df['targetPrice'] * (1 + fees)) - 1, 0)
+df['drr'] = np.where(df['high'] > df['targetPrice'], (df['close'] / (1 + fees)) / (df['targetPrice'] * (1 + fees)) - 1 , 0)
 
-df['crr'] = 1.0
-df['mdd'] = 0.0
-df['max_crr'] = 1.0
-for i in range(1, day_count) :
-    df['crr'][i] = df['crr'][i-1] * (1 + df['drr'][i])
-    df['max_crr'][i] = max(df['max_crr'][i-1], df['crr'][i])
-    df['mdd'][i] = (df['crr'][i] / df['max_crr'][i]) - 1
+df['crr'] = (df['drr'] + 1).cumprod() - 1
+df['mdd'] = -(((df['crr'] + 1).cummax() - (df['crr'] + 1)) / (df['crr'] + 1).cummax())
 
-print("기간수익률 :", df['crr'][-1] * 100 - 100, "% , 최대손실률 :", min(df['mdd']) * 100, "% , 수수료 :", fees * 100, "%")
+print("기간수익률 :", df['crr'][-1] * 100 - 100, "% , 최대손실률 :", df['mdd'].min() * 100, "% , 수수료 :", fees * 100, "%")
 print("알고리즘 적용 없을 시 수익률 :", ((df['close'][-1]/(1+fees))/(df['open'][0]*(1+fees))-1) * 100,"%")
 
 df.to_excel("crypto_history.xlsx")
