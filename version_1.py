0b61e5314b900dae45e6b97b7d2f60948fc22bd3
poArlim <dkfhscjsgk96@gmail.com>
2021-04-26 19:37:14 +0900

    remove duplicated variables

--- version_1.py
+++ version_1.py
@@ -1,6 +1,6 @@
 import pyupbit
-from datetime import datetime
-import time
+import datetime
+import time, calendar
 import auth             # save 'access key' and 'secret key'
 
 def get_targetPrice(df, K) :
@@ -35,31 +35,30 @@ if __name__ == '__main__':
         K = 0.5
     
         start_balance = upbit.get_balance("KRW")
-
-        sell_all(coin)
         df = pyupbit.get_ohlcv(coin, count = 2, interval = "day")
         targetPrice = get_targetPrice(df, K)
-        print(datetime.now().strftime('%y/%m/%d %H:%M:%S'), "\t\tBalance :", start_balance, "KRW \t\tYield :", ((start_balance / start_balance) - 1) * 100, "% \t\tNew targetPrice :", targetPrice, "KRW")
+        print(datetime.datetime.now().strftime('%y/%m/%d %H:%M:%S'), "\t\tBalance :", start_balance, "KRW \t\tYield :", ((start_balance / start_balance) - 1) * 100, "% \t\tNew targetPrice :", targetPrice, "KRW")
 
         while True :
-            if datetime.now().hour == 9 and datetime.now().minute == 2 and datetime.now().second == 0:    # when am 09:02:00
+            now = datetime.datetime.now()
+            if now.hour == 9 and now.minute == 2 and now.second == 0:    # when am 09:02:00
                 sell_all(coin)
                 df = pyupbit.get_ohlcv(coin, count = 2, interval = "day")
                 targetPrice = get_targetPrice(df, K)
                 
                 cur_balance = upbit.get_balance("KRW")
-                print(datetime.now().strftime('%y/%m/%d %H:%M:%S'), "\t\tBalance :", cur_balance, "KRW \t\tYield :", ((cur_balance / start_balance) - 1) * 100, "% \t\tNew targetPrice :", targetPrice, "KRW")
+                print(now.strftime('%y/%m/%d %H:%M:%S'), "\t\tBalance :", cur_balance, "KRW \t\tYield :", ((cur_balance / start_balance) - 1) * 100, "% \t\tNew targetPrice :", targetPrice, "KRW")
                 time.sleep(1)
             
             if targetPrice <= pyupbit.get_current_price(coin) :
-                buy_all(df, coin)
+                buy_all(coin)
                 
                 start_time = df.index[-1]
-                now = datetime.datetime.now()
                 end_time = start_time + datetime.timedelta(days=1)
                 if end_time > now :
+                    print("slee :",(end_time - now).seconds)
                     time.sleep((end_time - now).seconds)
-            
+    
             time.sleep(0.1)
 
     except Exception as e:
