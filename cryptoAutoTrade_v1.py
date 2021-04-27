4cae82021dda1df7d394f2d006dd86892c61e139
poArlim <dkfhscjsgk96@gmail.com>
2021-04-27 11:57:52 +0900

    complete version 1

--- cryptoAutoTrade_v1.py
+++ cryptoAutoTrade_v1.py
@@ -9,20 +9,14 @@ def get_targetPrice(df, K) :
 
 def buy_all(coin) :
     balance = upbit.get_balance("KRW") * 0.9995
-    if balance < 5000 :
-        print("not enough money")
-        return
-    print("buy order")
-    print(upbit.buy_market_order(coin, balance))
-    #upbit.buy_market_order(coin, balance)
+    if balance >= 5000 :
+        print(upbit.buy_market_order(coin, balance))
 
 def sell_all(coin) :
-    balance = upbit.get_balance(coin) * 0.9995
+    balance = upbit.get_balance(coin)
     price = pyupbit.get_current_price(coin)
-    if price * balance > 5000 :
-        print("sell order")
+    if price * balance >= 5000 :
         print(upbit.sell_market_order(coin, balance))
-        #upbit.sell_market_order(coin, balance)
 
 
 if __name__ == '__main__': 
@@ -41,25 +35,25 @@ if __name__ == '__main__':
 
         while True :
             now = datetime.datetime.now()
-            if now.hour == 9 and now.minute == 2 and now.second == 0:    # when am 09:02:00
+            if now.hour == 9 and now.minute == 0 and now.second == 0:    # when am 09:02:00
                 sell_all(coin)
                 df = pyupbit.get_ohlcv(coin, count = 2, interval = "day")
                 targetPrice = get_targetPrice(df, K)
-                
+                time.sleep(1)
+
                 cur_balance = upbit.get_balance("KRW")
                 print(now.strftime('%y/%m/%d %H:%M:%S'), "\t\tBalance :", cur_balance, "KRW \t\tYield :", ((cur_balance / start_balance) - 1) * 100, "% \t\tNew targetPrice :", targetPrice, "KRW")
-                time.sleep(1)
             
-            if targetPrice <= pyupbit.get_current_price(coin) :
+            elif targetPrice <= pyupbit.get_current_price(coin) :
                 buy_all(coin)
                 
                 start_time = df.index[-1]
                 end_time = start_time + datetime.timedelta(days=1)
                 if end_time > now :
-                    print("slee :",(end_time - now).seconds)
-                    time.sleep((end_time - now).seconds)
+                    print((end_time - now).seconds)
+                    time.sleep((end_time - now).seconds - 60)
     
-            time.sleep(0.1)
+            time.sleep(1)
 
     except Exception as e:
         print(e)
