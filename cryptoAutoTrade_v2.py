3a1791a0315c8141f216f2954778056f17863055
poArlim <dkfhscjsgk96@gmail.com>
2021-04-27 21:55:57 +0900

    complete cryptoAutoTrade version 2 adding best_K strategy.

--- cryptoAutoTrade_v2.py
+++ cryptoAutoTrade_v2.py
@@ -19,17 +19,17 @@ def sell_all(coin) :
     if price * balance >= 5000 :
         print(upbit.sell_market_order(coin, balance))
 
-def get_crr(df, fees, k) :
+def get_crr(df, fees, K) :
     df['range'] = df['high'].shift(1) - df['low'].shift(1)
     df['targetPrice'] = df['open'] + df['range'] * K
     df['drr'] = np.where(df['high'] > df['targetPrice'], (df['close'] / (1 + fees)) / (df['targetPrice'] * (1 + fees)) , 1)
     return df['drr'].cumprod()[-2]
 
-def get_best_K(coin, fees, count) :
-    df = pyupbit.get_ohlcv(coin, count = count, interval = "day")
+def get_best_K(coin, fees) :
+    df = pyupbit.get_ohlcv(coin, interval = "day", count = 21)
     max_crr = 0
     best_K = 0.5
-    for k in np.arange(0.1, 1.0, 0.1) :
+    for k in np.arange(0.0, 1.0, 0.1) :
         crr = get_crr(df, fees, k)
         if crr > max_crr :
             max_crr = crr
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     
         start_balance = upbit.get_balance("KRW")
         df = pyupbit.get_ohlcv(coin, count = 2, interval = "day")
-        targetPrice = get_targetPrice(df, K)
+        targetPrice = get_targetPrice(df, get_best_K(coin, fees))
         print(datetime.datetime.now().strftime('%y/%m/%d %H:%M:%S'), "\t\tBalance :", start_balance, "KRW \t\tYield :", ((start_balance / start_balance) - 1) * 100, "% \t\tNew targetPrice :", targetPrice, "KRW")
 
         while True :
@@ -55,7 +55,7 @@ if __name__ == '__main__':
             if now.hour == 9 and now.minute == 0 and now.second == 0:    # when am 09:02:00
                 sell_all(coin)
                 df = pyupbit.get_ohlcv(coin, count = 2, interval = "day")
-                targetPrice = get_targetPrice(df, K)
+                targetPrice = get_targetPrice(df, get_best_K(coin, fees))
                 time.sleep(1)
 
                 cur_balance = upbit.get_balance("KRW")
